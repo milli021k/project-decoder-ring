185e00c58f5cda89bf8c15fd67fb38da29b3fb10
unknown <milli2021k@gmail.com>
2020-12-28 18:54:14 -0800

    first polybius

--- src/polybius.js
+++ src/polybius.js
@@ -1,3 +1,25 @@
-function polybius(input, encode = true) {}
+function polybius(input, encode = true) {
+
+if (endcode===true)
+{
+    encode(input);
+}
+else
+decode(input);
+}
+
+const encode = (input)=>{
+
+    let grid = [
+        ['a', 'b','c','d','e'],
+        ['f','g','h','i/j','k' ],
+        ['l','m','n','o','p'],
+        ['q','r','s','t','u',],
+        ['v','w','x','y','z']
+    ];
+}
+
+
+
 
 module.exports = polybius;
