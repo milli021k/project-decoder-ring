0160f0ad916f93940f3afdc789b138c72dbd5bf5
unknown <milli2021k@gmail.com>
2020-12-28 22:07:52 -0800

    updated polybius

--- src/polybius.js
+++ src/polybius.js
@@ -1,42 +1,59 @@
 function polybius(input, encode = true) {
-
-if (endcode===true)
-{
-    encode(input);
-}
-else
-decode(input);
-}
-
-
-
-
-const encode = (input)=>{
-let result="";
-let allSmallInput = input.toLowwerCase();
-let grid = [
-        ['a', 'b','c','d','e'],
-        ['f','g','h','i/j','k' ],
-        ['l','m','n','o','p'],
-        ['q','r','s','t','u',],
-        ['v','w','x','y','z']
-    ];
-grid.forEach(row=>{
-  row.forEach(column=>{
-      
-if(column === 'a')
-{const rowIndex = row.indexOf('a');
-const ColumnIndex = column.indexOf('a');
-    result= result+column;
-    result= result+row;
-}
-  });
-    });
-
-
-}
-
-
-
-
+    let answer ="";
+     if (encode!=false)
+      {
+       answer = encoder(input);
+      }
+     else
+      {
+       answer = decode(input);
+      }
+    return answer;
+     
+   }
+     
+   // custom helper function 
+   function encoder(input){
+   
+   let result="";
+   let grid = [
+             ['#','#','#','#','#','#'],
+             ['#','a', 'b','c','d','e'],
+             ['#','f','g','h','i/j','k' ],
+             ['#','l','m','n','o','p'],
+             ['#','q','r','s','t','u',],
+             ['#','v','w','x','y','z']
+         ];
+   
+       let inputString =input.toLowerCase();   
+      for(let i=0 ; i<inputString.length;i++)
+      {
+       let letter = inputString[i];
+       if(letter==='i'||letter==='j')
+         {
+           letter ='i/j';  
+         }
+         if(letter===' ')
+         {
+           result = result+letter;
+           break;
+         }
+   
+      for(let row=0;row<grid.length;row++){
+       for(let column =0;column<6;column++)
+        {
+       
+         if(grid[row][column]===letter)
+          {
+           result=result+column;
+           result=result+row;
+         }
+       }
+   
+      }
+    }
+   return result;
+   
+   }
+   
 module.exports = polybius;
