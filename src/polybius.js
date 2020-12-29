5c017b19df5d7e646cc5489565ca4b2b2e913552
unknown <milli2021k@gmail.com>
2020-12-28 22:04:52 -0800

    added polybius

--- src/polybius.js
+++ src/polybius.js
@@ -8,15 +8,32 @@ else
 decode(input);
 }
 
-const encode = (input)=>{
 
-    let grid = [
+
+
+const encode = (input)=>{
+let result="";
+let allSmallInput = input.toLowwerCase();
+let grid = [
         ['a', 'b','c','d','e'],
         ['f','g','h','i/j','k' ],
         ['l','m','n','o','p'],
         ['q','r','s','t','u',],
         ['v','w','x','y','z']
     ];
+grid.forEach(row=>{
+  row.forEach(column=>{
+      
+if(column === 'a')
+{const rowIndex = row.indexOf('a');
+const ColumnIndex = column.indexOf('a');
+    result= result+column;
+    result= result+row;
+}
+  });
+    });
+
+
 }
 
 
