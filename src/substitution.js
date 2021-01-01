67446366d9586794aac7b960fbb3383242f2d988
unknown <milli2021k@gmail.com>
2020-12-31 18:00:58 -0800

    substitution

--- src/substitution.js
+++ src/substitution.js
@@ -1,3 +1,59 @@
-function substitution(input, alphabet, encode = true) {}
-
-module.exports = substitution;
+function substitution(input, alphabet, encode = true) { 
+    let inputString =input.toLowerCase();
+    let answer ="";
+    let orderdAplhabet = "abcdefghijklmnopqrstuvwxyz"
+    
+    if (encode!=false)
+      {
+       answer = encoder(inputString,alphabet,orderdAplhabet);
+      }
+     
+     else
+      {
+       answer = decoder(inputString,alphabet,orderdAplhabet);
+      }
+     
+    return answer;
+     
+   }
+   
+   // decoder
+   function decoder(inputString,alphabet,orderdAplhabet)
+   {
+     
+     let result ="";
+     for(let i=0;i<inputString.length;i++)
+       {
+         for(let j=0;j<alphabet.length;j++)
+           {
+             if(inputString[i]===alphabet[j])
+               {
+                 result =result+orderdAplhabet[j];
+               }
+           }
+       }
+     
+     return result;
+   }
+   
+   
+   // encode function 
+   function encoder(inputString,alphabet,orderdAplhabet)
+   {
+     let result ="";
+     for(let i=0;i<inputString.length;i++)
+       {
+         for(let j=0;j<orderdAplhabet.length;j++)
+           {
+             if(inputString[i]===orderdAplhabet[j])
+               {
+                 result =result+alphabet[j];
+               }
+           }
+       }
+     
+     return result;
+   }
+   
+   module.exports = substitution;
+   
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,23 @@
-// Write your tests here!
+const expect = require("chai").expect;
+const substitution = require("../src/substitution.js");
+
+describe("substitution" ,()=>{
+
+it("should encrypt if suplied input and alphabet",()=>{
+ const expected ="jrufscpw";
+  const actual = substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev");
+ expect(actual).to.be.equal(expected);
+});  
+
+
+  
+it("should decrypt if suplied input and alphabet",()=>{
+ const expected ="thinkful";
+  const actual = substitution("jrufscpw", "xoyqmcgrukswaflnthdjpzibev", false);  
+ expect(actual).to.be.equal(expected);
+});  
+
+
+});
+
+
