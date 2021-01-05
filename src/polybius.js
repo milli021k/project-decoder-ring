e2e57787b25286df46fbc48d044c0de414c28b17
unknown <milli2021k@gmail.com>
2021-01-05 11:24:21 -0800

    final commit

--- src/polybius.js
+++ src/polybius.js
@@ -1,94 +1,86 @@
 function polybius(input, encode = true) {
-    let inputString =input.toLowerCase();    
-    let answer ="";
-    let grid = [
-     ['#','#','#','#','#','#'],
-     ['#','a', 'b','c','d','e'],
-     ['#','f','g','h','(i/j)','k' ],
-     ['#','l','m','n','o','p'],
-     ['#','q','r','s','t','u',],
-     ['#','v','w','x','y','z']
-   ];
-   
-    if (encode!=false)
-      {
-       answer = encoder(inputString,grid);
-      }
-     else
-      {
-       answer = decoder(inputString,grid);
-      }
-    return answer;
-     
-   }
-     
-   // custom helper function 
-   function encoder(inputString,grid){
-   
-   let result="";
-      for(let i=0 ; i<inputString.length;i++)
-      {
-       let letter = inputString[i];
-       if(letter==='i'||letter==='j')
-         {
-           letter ='(i/j)';  
-         }
-         if(letter===' ')
-         {
-           result = result+letter;
-           continue;
-         }
-   
-      for(let row=0;row<grid.length;row++){
-       for(let column =0;column<6;column++)
-        {
-       
-         if(grid[row][column]===letter)
-          {
-           result=result+column;
-           result=result+row;
-         }
-       }
-   
+  let inputString = input.toLowerCase();
+  let answer = "";
+  let grid = [
+    ["#", "#", "#", "#", "#", "#"],
+    ["#", "a", "b", "c", "d", "e"],
+    ["#", "f", "g", "h", "(i/j)", "k"],
+    ["#", "l", "m", "n", "o", "p"],
+    ["#", "q", "r", "s", "t", "u"],
+    ["#", "v", "w", "x", "y", "z"],
+  ];
+
+  if (encode != false) {
+    answer = encoderPolybius(inputString, grid);
+  } else {
+    answer = decoderPolybius(inputString, grid);
+  }
+  return answer;
+}
+
+// custom helper function
+function encoderPolybius(inputString, grid) {
+  let result = "";
+
+  for (let index = 0; index < inputString.length; index++) {
+    let character = inputString[index];
+
+    if (character === " ") {
+      result = result + character;
+      continue;
+    }
+
+    if (!/[a-zA-Z]/.test(character)) {
+      return false;
+    }
+
+    if (character === "i" || character === "j") {
+      character = "(i/j)";
+    }
+
+    for (let row = 0; row < grid.length; row++) {
+      for (let column = 0; column < 6; column++) {
+        if (grid[row][column] === character) {
+          result = result + column;
+          result = result + row;
+        }
       }
     }
-   return result;
-   }
-   
-   
-   // function to decode a number to string 
-   
-   function decoder(inputString,grid){
-   
-   let processedString = inputString.split(" ");
-   let removedSpaces =processedString.join('');
-   if((removedSpaces.length%2)!=0)
-   {
-     return false;
-   }
-   let result ="";
-     
-   for(let i=0 ; i<inputString.length;i=i+2)
-   {
-    let letterCoordinate = inputString[i]+inputString[i+1];
-   
-    if(letterCoordinate[0]===' ')
-      { i =i-1;
-        result = result+letterCoordinate[0];
-        continue;
-      }
-   
-   for(let row=0;row<grid.length;row++){
-    for(let column =0;column<6;column++)
-     {
-     if(row===parseInt(letterCoordinate[0])&&column===parseInt(letterCoordinate[1]))
-       {
-        result=result+grid[column][row];
+  }
+  return result;
+}
+
+// function to decode a number to string
+
+function decoderPolybius(inputString, grid) {
+  let processedString = inputString.split(" ");
+  let removedSpaces = processedString.join("");
+  if (removedSpaces.length % 2 != 0) {
+    return false;
+  }
+
+  let result = "";
+
+  for (let index = 0; index < inputString.length; index = index + 2) {
+    let letterCoordinate = inputString[index] + inputString[index + 1];
+
+    if (letterCoordinate[0] === " ") {
+      index = index - 1;
+      result = result + letterCoordinate[0];
+      continue;
+    }
+
+    for (let row = 0; row < grid.length; row++) {
+      for (let column = 0; column < 6; column++) {
+        if (
+          row === parseInt(letterCoordinate[0]) &&
+          column === parseInt(letterCoordinate[1])
+        ) {
+          result = result + grid[column][row];
+        }
       }
     }
-   }
-   }
-     return result;
-   }
-   module.exports = polybius;
-   
+  }
+  return result;
+}
+module.exports = polybius;
--- src/substitution.js
+++ src/substitution.js
@@ -1,101 +1,69 @@
-function substitution(input, alphabet, encode = true) { 
-  let inputString =input.toLowerCase();
-  let answer ="";
-  let orderdAplhabet = "abcdefghijklmnopqrstuvwxyz"
-  if(alphabet.length != 26|| typeof(alphabet)!= 'string')
-    {
-      return false;
-    }
+function substitution(input, alphabet, encode = true) {
+  let inputString = input.toLowerCase();
+  let answer = "";
+  let orderdAplhabet = "abcdefghijklmnopqrstuvwxyz";
+  if (alphabet.length != 26 || typeof alphabet != "string") {
+    return false;
+  }
   let check = checkDuplicate(alphabet);
-  if(check)
-     {
-      return false; 
-     }
-  
-  if (encode!=false)
-    {
-     answer = encoder(inputString,alphabet,orderdAplhabet);
-    }
-   
-   else
-    {
-     answer = decoder(inputString,alphabet,orderdAplhabet);
-    }
-   
+  if (check) {
+    return false;
+  }
+
+  if (encode != false) {
+    answer = encoderSubstitution(inputString, alphabet, orderdAplhabet);
+  } else {
+    answer = decoderSubstitution(inputString, alphabet, orderdAplhabet);
+  }
+
   return answer;
-   
- }
- 
- // decoder
- function decoder(inputString,alphabet,orderdAplhabet)
- {
-   
-   let result ="";
-   for(let i=0;i<inputString.length;i++)
-     {
-       
-       if(inputString[i]===' ')
-       {
-         result = result+inputString[i];
-         //continue;
-       }
-       for(let j=0;j<alphabet.length;j++)
-         {
-           if(inputString[i]===alphabet[j])
-             {
-               result =result+orderdAplhabet[j];
-             }
-         }
-     }
-   
-   return result;
- }
- 
- 
- // encode function 
- function encoder(inputString,alphabet,orderdAplhabet)
- {
-   let result ="";
-   
-   
-   for(let i=0;i<inputString.length;i++)
-     {
-       if(inputString[i]===' ')
-       {
-         result = result+inputString[i];
-         //continue;
-       }
-       for(let j=0;j<orderdAplhabet.length;j++)
-         {
-           if(inputString[i]===orderdAplhabet[j])
-             {
-               result =result+alphabet[j];
-             }
-         }
-     }
-   
-   return result;
- }
- 
- 
- function checkDuplicate(input)
- {
-   let answer =false;
- for(let i =0 ;i<input.length;i++)
- {
-  for(let j=i+1;j<input.length;j++)
-  {
-    if(input[i]===input[j])
-    {
-     answer = true;
+}
+
+// decoder
+function decoderSubstitution(inputString, alphabet, orderdAplhabet) {
+  let result = "";
+  for (let i = 0; i < inputString.length; i++) {
+    if (inputString[i] === " ") {
+      result = result + inputString[i];
+    }
+    for (let j = 0; j < alphabet.length; j++) {
+      if (inputString[i] === alphabet[j]) {
+        result = result + orderdAplhabet[j];
+      }
     }
   }
- }
- return answer;
- }
- 
- 
- 
- 
- module.exports = substitution;
- 
+
+  return result;
+}
+
+// encode function
+function encoderSubstitution(inputString, alphabet, orderdAplhabet) {
+  let result = "";
+
+  for (let i = 0; i < inputString.length; i++) {
+    if (inputString[i] === " ") {
+      result = result + inputString[i];
+    }
+    for (let j = 0; j < orderdAplhabet.length; j++) {
+      if (inputString[i] === orderdAplhabet[j]) {
+        result = result + alphabet[j];
+      }
+    }
+  }
+
+  return result;
+}
+
+function checkDuplicate(input) {
+  let answer = false;
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        answer = true;
+      }
+    }
+  }
+  return answer;
+}
+
+module.exports = substitution;
